90ff4788d27cea40f2716ff7a979770f4d3898ca
Freddy Pastaff <amoakof92@gmail.com>
2025-10-24 14:42:46 +0000

    Fix case quantity to quantity DOUBLE

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS Books (
     title VARCHAR(130) NOT NULL,
     author_id INT,
     publication_date DATE ,
-    price DOUDLE NOT NULL,
+    price DOUBLE NOT NULL,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 
 );
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity INT NOT NULL,
+    quantity DOUBLE INT NOT NULL,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );

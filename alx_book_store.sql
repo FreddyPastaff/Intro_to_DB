f177ab706f18c595bcbfcb8121cae3728e1df539
Freddy Pastaff <amoakof92@gmail.com>
2025-10-24 14:39:06 +0000

    Fix casing of 'text' to 'TEXT' for address column in Customers table

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE
-    address text
+    address TEXT
 );
 
 -- Create the orders table

b1bbfc4756ed7cae7be475d1cc1354a9231ed8ca
Freddy Pastaff <amoakof92@gmail.com>
2025-10-24 16:00:26 +0000

    task_2_v1.4

--- task_2.sql
+++ task_2.sql
@@ -27,7 +27,7 @@ CREATE TABLE Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE order_Details (
+CREATE TABLE Order_Details (
     order_detail_id INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT,
     book_id INT,
